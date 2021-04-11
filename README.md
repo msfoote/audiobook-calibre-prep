d3968ae24b9d00ff0ab948eb00b424677239fce9
Matthew <matthew.foote@gmail.com>
2021-04-11 08:11:04 -0700

    Revised ZIP_Files to take in an argument and revised README.md to make note of this

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # audiobook-calibre-prep
+
 A set of scripts used to prep audiobooks for import into a Calibre Library
+
+As of the writing of this there is no good way to host and share audiobook files across mutliple devices. This set of scripts was meant to complete the final step in a workflow that took me from audiobook files to accessing audiobooks from anywhere.
+
+My workflow consists of the following steps:
+
+1. Procure the audiobook files
+2. Properly tag the audiobook files. While not relevant to this script, I adjust the tags as follows with [mp3tag](https://www.mp3tag.de/en/):
+    - Number the tracks making sure to add leading zeros depending on the total number of tracks
+    - I make sure the `Artist` and the `Album Artist` fields both reflect the authors name.  In the case of multiple authors I typically choose one.
+    - The `Album` is the name of the audiobook
+    - Unless the titles for the individual files are meaningful I change them using the `Tag - Tag` feature to
+        >TITLE = $num(%track%,3) - %album%
+    - Add an image to the files that represents the cover.
+    - When done tagging the files I move and rename them using the `Tag - Filename` feature and the following code:
+        >\\`Directory for Corrected Audiobooks`\\%album% - %artist%\$num(%track%,3) - %album%
+3. Run `Zip_Files.py` on the directory noted above which will create one zip file for each audiobook.
+
+## Usage
+
+### `Zip_Files.py`
+
+This script takes in a single argument which is the parent directory where your corrected books have been stored. This directory should only contain directories representing each audiobook to be processed.
+
+>Zip_Files.py -d="`Some Directory`"
+
+The script will ZIP up the contents of each directory and then delete the subdirectory. You will be left with initially specified directory containing a number of ZIP files. These you can then upload into Calibre to be shared how you wish. I currently use [Calibre-Web](https://github.com/janeczku/calibre-web)
--- Zip_Files.py
+++ Zip_Files.py
@@ -1,32 +1,42 @@
 from zipfile import ZipFile
 import os, shutil
+import argh
+from tqdm import tqdm, trange
 
-directory = os.getcwd()
+# TODO Make sure that if the zip files are already there the program skips the book or stops
+def main(directory=None):
+    # TODO Test to make sure this works when no argument is given
+    if directory is None:
+        directory = os.getcwd()
+    
+    print('This will zip all of the files in each subdirectory of ' + directory + ' into their own zip file')
+    booknames = []
+    bookdirs = []
+    for root, dirs, files in os.walk(directory):
+        if root == directory:
+            tot_dirs = len(dirs)
+            tot_files = len(files)
+            for i, name in enumerate(dirs):
+                bookname = name
+                booknames.append(bookname)
+                bookdir = os.path.join(root, name)
+                bookdirs.append(bookdir)
 
-print('This will zip all of the files in each subdirectory of ' + directory + ' into their own zip file')
-booknames = []
-bookdirs = []
-for root, dirs, files in os.walk(directory):
-    if root == directory:
-        tot_dirs = len(dirs)
-        tot_files = len(files)
-        for i, name in enumerate(dirs):
-            bookname = name
-            booknames.append(bookname)
-            bookdir = os.path.join(root, name)
-            bookdirs.append(bookdir)
+    for i, bookdir in enumerate(bookdirs):
+        bookname = booknames[i]
+        for j, (root, dirs, files) in enumerate(os.walk(bookname)):
+            if j==0:
+                with ZipFile('{0}.zip'.format(os.path.join(directory, bookname)), 'w') as zip:
+                    # TODO Implement progress bars using TQDM.  Refer to https://www.youtube.com/watch?v=eILeIEE3C8c&t=311s
+                    print('\tNow zipping book ' + str(i + 1) + ' of ' + str(tot_dirs) + ': ' + bookname)
+                    # print(zip.filename)
+                    for file in files:
+                        if file.endswith(('.mp3', '.m4b', '.aa', '.aax', '.m4a', '.aac', '.m4p', '.ogg', '.wma', '.flac',
+                                        '.alac', '.wav', 'm3u', '.py')):
+                            # print('\t\t',os.path.join(directory,root,file))
+                            test = os.path.join(directory, root, file)
+                            zip.write(test, os.path.basename(test))
+        shutil.rmtree(bookdir)
 
-for i, bookdir in enumerate(bookdirs):
-    bookname = booknames[i]
-    for j, (root, dirs, files) in enumerate(os.walk(bookname)):
-        if j==0:
-            with ZipFile('{0}.zip'.format(os.path.join(directory, bookname)), 'w') as zip:
-                print('\tNow zipping book ' + str(i + 1) + ' of ' + str(tot_dirs) + ': ' + bookname)
-                # print(zip.filename)
-                for file in files:
-                    if file.endswith(('.mp3', '.m4b', '.aa', '.aax', '.m4a', '.aac', '.m4p', '.ogg', '.wma', '.flac',
-                                      '.alac', '.wav', 'm3u', '.py')):
-                        # print('\t\t',os.path.join(directory,root,file))
-                        test = os.path.join(directory, root, file)
-                        zip.write(test, os.path.basename(test))
-    shutil.rmtree(bookdir)
+if __name__ == "__main__":
+    argh.dispatch_command(main)
